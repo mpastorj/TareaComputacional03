268857f461257874e4e7b8c3e4ab798bcb07a096
MonicaRiquelme <monica.riquelme@ceinf.cl>
2013-04-10 21:45:26 -0300

    Consulta a la base de datos

--- main.cpp
+++ main.cpp
@@ -12,5 +12,16 @@ cn.conectar();
 
 cn.consulta("SELECT * FROM asignaturas_cursadas");
 
+select c.docente_id, avg (a.nota), stdev(a.nota)
+from asignaturas_cursadas a, cursos c
+where c.curso_id = a.curso_id
+group by c.docente_id;
+
+select c.curso_id, c.anio, c.semestre, c.seccion, a.ac_id
+from asignaturas_cursadas a, cursos c
+where c.curso_id = a.curso_id
+order by c.anio asc, c.semestre desc;
+
+
 
 }
